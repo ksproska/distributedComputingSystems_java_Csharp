06c17ddb66c86488fa0cc69038e8c038a312c202
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-03-13 10:25:33 +0100

    cw1 documentation

--- RSI_cw1/src/ClientRPC.java
+++ RSI_cw1/src/ClientRPC.java
@@ -3,17 +3,31 @@ import java.util.Vector;
 
 public class ClientRPC {
     public static void main(String[] args) {
-//        AC cb = new AC();
         try {
             // TODO: xxx -> 10000 + numer komputera w laboratorium
+
+            // kod utworzenia obiektu wywołania metody serwera,
             XmlRpcClient srv = new XmlRpcClient("http://localhost:xxx");
+
+//            AC cb = new AC();
+//            Vector<Integer> params2 = new Vector<Integer>();
+//            params2.addElement(new Integer(3000));
+//            srv.executeAsync("MojSerwer.execAsy", params2, cb);
+//            System.out.println("Wywolano asynchronicznie");
+
+            // upakowania parametrów dla wywołania metody echo,
             Vector<Integer> params = new Vector<Integer>();
             params.addElement(new Integer(13));
             params.addElement(new Integer(21));
+
+            // wywołania samej metody,
             Object result = srv.execute("MojSerwer.echo", params);
+
+            //Dodaj także wyświetlanie otrzymanej wartość.
             int wynik = ((Integer) result).intValue();
             System.out.println(wynik);
-        } catch (Exception exception) {
+        }
+        catch (Exception exception) {
             System.err.println("Klient XML-RPC: " +exception);
         }
     }
--- RSI_cw1/src/SerwerRPC.java
+++ RSI_cw1/src/SerwerRPC.java
@@ -1,15 +1,40 @@
 import org.apache.xmlrpc.WebServer;
 
 public class SerwerRPC {
+
+    /**
+     * zwracała sumę dwóch zmiennych całkowitych podanych jako parametry wywołania metody
+     */
     public Integer echo(int x, int y) {
         return x + y;
     }
+
+    /**
+     * Procedura asynchroniczna, będzie teoretycznie długo coś wykonywać,
+     * co zasymulowane będzie wstrzymaniem wykonywania na okres x milisekund
+     *
+     * @param x parametr podawany w wywołaniu procedury) metodą Thread.sleep(x)
+     * @return
+     */
+    public int execAsy(int x) {
+        System.out.println("... wywołano asy - odliczam");
+        try {
+            Thread.sleep(x);
+        } catch(InterruptedException ex) {
+            ex.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("... asy - koniec odliczania");
+        return (123);
+    }
+
     public static void main(String[] args) {
         try {
             System.out.println("Startuje serwer XML-RPC...");
             // TODO: xxx -> 10000 + numer komputera w laboratorium
             int port = 10000;
             WebServer server = new WebServer(port);
+            //ponizej tworzy się obiekt swojej klasy serwera i uruchomia się go:
             server.addHandler("MojSerwer", new SerwerRPC());
             server.start();
             System.out.println("Serwer wystartowal pomyslnie.");
@@ -21,3 +46,5 @@ public class SerwerRPC {
         }
     }
 }
+
+// KAMILA STACJONARNY: java -cp "F:\rozproszone_systemy\xmlrpc-1.2-b1.jar" "F:\rozproszone_systemy\RSI_cw1\src\SerwerRPC.java"
