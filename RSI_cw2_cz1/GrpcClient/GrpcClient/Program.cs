af832fe4721b91764632ed5757ef1e3a907c43a2
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-03-25 11:35:04 +0100

    server and client work, but for default greeter configuration

--- RSI_cw2_cz1/GrpcClient/GrpcClient/Program.cs
+++ RSI_cw2_cz1/GrpcClient/GrpcClient/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting gRPC Client");
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            Console.Write("Enter the name: ");
+            String str = Console.ReadLine();
+            var reply = await client.SayHelloAsync(new HelloRequest
+            {
+                Name = str
+            });
+            Console.WriteLine("From server: " + reply.Message + "!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            channel.ShutdownAsync().Wait();
         }
     }
 }
