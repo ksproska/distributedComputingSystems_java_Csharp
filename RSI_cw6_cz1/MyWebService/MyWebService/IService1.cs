bc6603d9e2772f0c8397c908a238cea0e320a731
mkuchciak <mkuchciak14@gmail.com>
2022-05-21 23:47:28 +0200

    retyped

--- RSI_cw6_cz1/MyWebService/MyWebService/IService1.cs
+++ RSI_cw6_cz1/MyWebService/MyWebService/IService1.cs
@@ -44,4 +44,39 @@ namespace MyWebService
             set { stringValue = value; }
         }
     }
+
+    // Zdefiniuj kontrakt serwisu
+    [ServiceContract]
+    public interface IRestService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/books")]
+        List<Book> getAllXml();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/books/{id}", ResponseFormat = WebMessageFormat.Xml)]
+        Book getByIdXml(string Id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/books", Method = "POST", ResponseFormat = WebMessageFormat.Xml)]
+        string addXml(Book item);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/books/{id}", Method = "DELETE")] //"Xxx{id}"
+        string deleteXml(string Id);
+    }
+
+    // Zdefiniuj kontrakt danych
+    [DataContract]
+    public class Book
+    {
+        [DataMember(Order = 0)]
+        public int Id { get; set; }
+
+        [DataMember(Order = 1)]
+        public string Title { get; set; }
+
+        [DataMember(Order = 2)]
+        public double Price { get; set; }
+    }
 }
---
+++ RSI_cw6_cz1/MyWebService/MyWebService/Service1.svc.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MyWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class MyRestService : IRestService
+    {
+        private static List<Book> books_list = new List<Book>() {
+            new Book {Id = 100, Title = "Dziady", Price = 32.55},
+            new Book {Id = 101, Title = "Potop", Price = 15.00},
+            new Book {Id = 102, Title = "Balladyna", Price = 42.90}
+        };
+
+        public string addXml(Book item)
+        {
+            if (item == null)
+                throw new WebFaultException<string>("400: Bad Request", System.Net.HttpStatusCode.BadRequest);
+            int newIdx = item.Id;
+            int idx = books_list.FindIndex(b => b.Id == newIdx);
+            if (idx == -1)
+            {
+                item.Id = newIdx;
+                books_list.Add(item);
+                return "Added item with ID=" + item.Id;
+            }
+            else
+                throw new WebFaultException<string>("409: Conflict", System.Net.HttpStatusCode.Conflict);
+        }
+
+        public string deleteXml(string Id)
+        {
+            int intId = int.Parse(Id);
+            int idx = books_list.FindIndex(b => b.Id == intId);
+            if (idx == -1)
+                throw new WebFaultException<string>("404: Not Found", System.Net.HttpStatusCode.NotFound);
+            books_list.RemoveAt(idx);
+            return "Removed item with ID=" + Id;
+        }
+
+        public List<Book> getAllXml()
+        {
+            return books_list;
+        }
+
+        public Book getByIdXml(string Id)
+        {
+            int intId = int.Parse(Id);
+            int idx = books_list.FindIndex(b => b.Id == intId);
+            if (idx == -1)
+                throw new WebFaultException<string>("404: Not Found", System.Net.HttpStatusCode.NotFound);
+            return books_list.ElementAt(idx);
+        }
+    }
+}
