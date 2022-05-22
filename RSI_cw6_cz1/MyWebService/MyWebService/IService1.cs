b567416f3828f1f644d844cf81a18eadab0da4c6
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-05-23 00:01:20 +0200

    6 works

--- RSI_cw6_cz1/MyWebService/MyWebService/IService1.cs
+++ RSI_cw6_cz1/MyWebService/MyWebService/IService1.cs
@@ -8,43 +8,6 @@ using System.Text;
 
 namespace MyWebService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
-    //[ServiceContract]
-    //public interface IService1
-    //{
-
-    //    [OperationContract]
-    //    string GetData(int value);
-
-    //    [OperationContract]
-    //    CompositeType GetDataUsingDataContract(CompositeType composite);
-
-    //    // TODO: Add your service operations here
-    //}
-
-
-    //// Use a data contract as illustrated in the sample below to add composite types to service operations.
-    //[DataContract]
-    //public class CompositeType
-    //{
-    //    bool boolValue = true;
-    //    string stringValue = "Hello ";
-
-    //    [DataMember]
-    //    public bool BoolValue
-    //    {
-    //        get { return boolValue; }
-    //        set { boolValue = value; }
-    //    }
-
-    //    [DataMember]
-    //    public string StringValue
-    //    {
-    //        get { return stringValue; }
-    //        set { stringValue = value; }
-    //    }
-    //}
-
     // Zdefiniuj kontrakt serwisu
     [ServiceContract]
     public interface IRestService
@@ -54,16 +17,32 @@ namespace MyWebService
         List<Book> getAllXml();
 
         [OperationContract]
+        [WebGet(UriTemplate = "/json/books", ResponseFormat = WebMessageFormat.Json)]
+        List<Book> getAllJson();
+
+        [OperationContract]
         [WebGet(UriTemplate = "/books/{id}", ResponseFormat = WebMessageFormat.Xml)]
         Book getByIdXml(string Id);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/json/books/{id}", ResponseFormat = WebMessageFormat.Json)]
+        Book getByIdJson(string Id);
 
         [OperationContract]
         [WebInvoke(UriTemplate = "/books", Method = "POST", ResponseFormat = WebMessageFormat.Xml)]
         string addXml(Book item);
 
         [OperationContract]
+        [WebInvoke(UriTemplate = "/json/books", Method = "POST", ResponseFormat = WebMessageFormat.Json)]
+        string addJson(Book item);
+
+        [OperationContract]
         [WebInvoke(UriTemplate = "/books/{id}", Method = "DELETE")] //"Xxx{id}"
         string deleteXml(string Id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/json/books/{id}", Method = "DELETE", ResponseFormat = WebMessageFormat.Json)] //"Xxx{id}"
+        string deleteJson(string Id);
     }
 
     // Zdefiniuj kontrakt danych
--- RSI_cw6_cz1/MyWebService/MyWebService/Service1.svc.cs
+++ RSI_cw6_cz1/MyWebService/MyWebService/Service1.svc.cs
@@ -8,29 +8,6 @@ using System.Text;
 
 namespace MyWebService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    //public class Service1 : IService1
-    //{
-    //    public string GetData(int value)
-    //    {
-    //        return string.Format("You entered: {0}", value);
-    //    }
-
-    //    public CompositeType GetDataUsingDataContract(CompositeType composite)
-    //    {
-    //        if (composite == null)
-    //        {
-    //            throw new ArgumentNullException("composite");
-    //        }
-    //        if (composite.BoolValue)
-    //        {
-    //            composite.StringValue += "Suffix";
-    //        }
-    //        return composite;
-    //    }
-    //}
-
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class Service1 : IRestService
     {
@@ -78,6 +55,26 @@ namespace MyWebService
             if (idx == -1)
                 throw new WebFaultException<string>("404: Not Found", System.Net.HttpStatusCode.NotFound);
             return books_list.ElementAt(idx);
+        }
+
+        string IRestService.addJson(Book item)
+        {
+            return addXml(item);
+        }
+
+        string IRestService.deleteJson(string Id)
+        {
+            return deleteXml(Id);
+        }
+
+        List<Book> IRestService.getAllJson()
+        {
+            return getAllXml();
+        }
+
+        Book IRestService.getByIdJson(string Id)
+        {
+            return getByIdXml(Id);
         }
     }
 }
