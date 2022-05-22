939440c10dac6fd645f38de8c1c19c8702c9a4b4
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-05-22 23:40:56 +0200

    6.3 - all comends work

--- RSI_cw6_cz1/MyWebService/MyWebService/Service1.svc.cs
+++ RSI_cw6_cz1/MyWebService/MyWebService/Service1.svc.cs
@@ -10,29 +10,29 @@ namespace MyWebService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
-    {
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
+    //public class Service1 : IService1
+    //{
+    //    public string GetData(int value)
+    //    {
+    //        return string.Format("You entered: {0}", value);
+    //    }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
-    }
+    //    public CompositeType GetDataUsingDataContract(CompositeType composite)
+    //    {
+    //        if (composite == null)
+    //        {
+    //            throw new ArgumentNullException("composite");
+    //        }
+    //        if (composite.BoolValue)
+    //        {
+    //            composite.StringValue += "Suffix";
+    //        }
+    //        return composite;
+    //    }
+    //}
 
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
-    public class MyRestService : IRestService
+    public class Service1 : IRestService
     {
         private static List<Book> books_list = new List<Book>() {
             new Book {Id = 100, Title = "Dziady", Price = 32.55},
